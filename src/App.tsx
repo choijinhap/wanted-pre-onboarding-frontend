d880c71f41408cd4874bbfcbb452364bf2229cbc
choijinhap <wlsgkq123@gmail.com>
2023-01-30 15:49:36 +0900

    chore : 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
+const Btn = styled.button`
+	background-color: yellow;
+	padding: 5px;
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Btn>asd</Btn>
+			<header
+				className="App-header"
+				css={css`
+					background-color: red;
+				`}
+			>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
