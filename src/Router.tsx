273fa78c31e927e9ad1622dbebe7a4054b416f52
choijinhap <wlsgkq123@gmail.com>
2023-02-01 17:03:57 +0900

    feat : 페이지구조 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Main from 'pages/Main';
+import SignIn from 'pages/SignIn';
+import SignUp from 'pages/SignUp';
+import Todo from 'pages/Todo';
+import Error from 'pages/Error';
+
 export default function Router() {
 	return (
 		<Routes>
-			<Route path="/" element={<div>main</div>} />
-			<Route path="/signup" element={<div>signup</div>} />
-			<Route path="/signin" element={<div>signin</div>} />
+			<Route path="/" element={<Main />} />
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/signin" element={<SignIn />} />
+			<Route path="/todo" element={<Todo />} />
+			<Route path="*" element={<Error />} />
 		</Routes>
 	);
 }
