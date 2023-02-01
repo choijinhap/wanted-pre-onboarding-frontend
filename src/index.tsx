dc0816834c1cf806d43014355f6577d9db46711c
choijinhap <wlsgkq123@gmail.com>
2023-02-01 17:22:52 +0900

    feat : global reset css 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Global } from '@emotion/react';
+
+import reset from 'styles/reset';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
+
 root.render(
 	<React.StrictMode>
+		<Global styles={reset} />
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
