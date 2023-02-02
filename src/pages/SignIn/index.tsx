6839cc57ed3b59d2d580275d7a9c5bcdbe512660
choijinhap <wlsgkq123@gmail.com>
2023-02-02 15:44:01 +0900

    feat : signin페이지 레이아웃 작성

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,52 @@
 import * as React from 'react';
 
+import SignForm from 'components/common/SignForm';
+
+type FormState = {
+	email: string;
+	password: string;
+};
+const initialFormState = {
+	email: '',
+	password: '',
+};
 export default function SignIn() {
-	return <div>SignIn</div>;
+	const [formState, setFormState] = React.useState<FormState>(initialFormState);
+	const onFormInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormState((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}));
+	};
+	const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+	return (
+		<SignForm onSubmit={onFormSubmit}>
+			<h1>로그인</h1>
+			<label htmlFor="email">
+				<span>이메일</span>
+				<input
+					type="email"
+					id="email"
+					data-testid="email-input"
+					value={formState.email}
+					onChange={onFormInputChange}
+				/>
+			</label>
+			<label htmlFor="password">
+				<span>비밀번호</span>
+				<input
+					type="password"
+					id="password"
+					data-testid="password-input"
+					value={formState.password}
+					onChange={onFormInputChange}
+				/>
+			</label>
+			<button type="submit" data-testid="signin-button">
+				로그인
+			</button>
+		</SignForm>
+	);
 }
